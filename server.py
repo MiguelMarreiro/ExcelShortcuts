e9537137a43a053a1445b1663cf8c88c7f23f5a9
MiguelMarreiro <miguel.marreiro>
2024-07-24 17:50:11 +0100

    add formulas, structure page, add index background

--- server.py
+++ server.py
@@ -4,6 +4,13 @@ app = Flask(__name__)
 @app.route("/")
 def landing_page():
     return render_template("index.html")
+@app.route("/shortcuts")
+def shortcuts_page():
+    return render_template("shortcuts.html")
+
+@app.route("/formulas")
+def formulas_page():
+    return render_template("formulas.html")
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
