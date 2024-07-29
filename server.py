0bed5110a0c56e3e380608f8ba20853a618bf299
MiguelMarreiro <miguel.marreiro>
2024-07-29 18:59:22 +0100

    add jinja documentation and cheatsheet using send_from_directory for sending static html instead of template

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 app = Flask(__name__)
 
 @app.route("/")
@@ -11,6 +11,18 @@ def shortcuts_page():
 @app.route("/formulas")
 def formulas_page():
     return render_template("formulas.html")
+
+@app.route("/jinja_documentation")
+def vs_shortcuts():
+    # return render_template("jinja_documentation.html")
+    return send_from_directory('templates', "jinja_documentation.html")
+
+@app.route("/jinja_cheatsheet")
+def vs_cheatsheet():
+    # return render_template("jinja_cheatsheet.html")
+    return send_from_directory('templates', "jinja_cheatsheet.html")
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
